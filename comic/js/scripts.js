0508df7fb5d4df1a183a11393f72eaef0bc6d98e
abhijeetdeb <abhijeetdeb@live.com>
2015-04-02 15:53:18 +0530

    Update

--- comic/js/scripts.js
+++ comic/js/scripts.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
-  $('aside > .close').on({
-    mouseenter: function() {
-      $('aside.fun').css({opacity: '0.5'});
-    },
-    mouseleave: function() {
-      $('aside.fun').css({opacity: '4'});
-    },
-    click: function() {
-      $('aside.fun').hide();
-    }
-  })
+  
+  function funFact() {
+    var $asideFun = $('aside.fun');
+    $('aside > .close').on({
+      mouseenter: function() {
+        $asideFun.css({opacity: '0.5'});
+      },
+      mouseleave: function() {
+        $asideFun.css({opacity: '1'});
+      },
+      click: function() {
+        $asideFun.hide();
+      }
+    });
+  }  
+  funFact();
+  
 });
