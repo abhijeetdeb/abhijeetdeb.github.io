50b01ea63fff8ba460728353b66a5486baff52dd
abhijeetdeb <abhijeetdeb@live.com>
2015-04-02 14:33:53 +0530

    Update

--- comic/js/scripts.js
+++ comic/js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  
+  $('aside > .close').on({
+    mouseenter: function() {
+      $('aside.fun').css({opacity: '0.5'});
+    },
+    mouseleave: function() {
+      $('aside.fun').css({opacity: '4'});
+    },
+    click: function() {
+      $('aside.fun').hide();
+    }
+  })
 });
