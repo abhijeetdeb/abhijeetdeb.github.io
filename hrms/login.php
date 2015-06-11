5c595b4170b776158038a95bd291fc553f1181b0
abhijeetdeb <abhijeetdeb@live.com>
2015-06-11 17:51:59 +0530

    Update

--- hrms/login.php
+++ hrms/login.php
@@ -3,12 +3,15 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>HRMS Beta</title>
+    <title>HRMS</title>
     <meta name="description" content="Abhijeet Deb">
     <meta name="author" content="Abhijeet Deb">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/styles.css">
     <!--if lt IE 9 <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>-->
   </head>
-  <body></body>
+  <body>
+    <?php echo "test" ?>
+    <?php echo "test" ?>
+  </body>
 </html>
